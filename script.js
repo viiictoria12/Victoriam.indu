c4cf1edbbd338f0b8b3a1b77af105b49414c5691
Victoria Moscardini <victoriamoscardini02@gmail.com>
2024-04-29 01:22:48 -0300

    mensaje a wsp

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-const puppeteer = require('puppeteer');
-
-async function sendMessageToInstagram(username, message) {
-    const browser = await puppeteer.launch({ headless: false }); // Para visualizar el navegador en acción, puedes cambiar a true para modo headless
-    const page = await browser.newPage();
-
-    // Ir a la página de Instagram e iniciar sesión manualmente
-    await page.goto('https://www.instagram.com/victoriam.indu/');
-    // Aquí debes implementar el código para iniciar sesión manualmente
-
-    // Ir al perfil del usuario al que quieres enviar el mensaje
-    await page.goto(`https://www.instagram.com/victoriam.indu/${username}/`);
-    await page.waitForSelector('button[type="button"]'); // Esperar a que se cargue el botón de mensaje
-    await page.click('button[type="button"]'); // Hacer clic en el botón de mensaje
-
-    // Escribir y enviar el mensaje
-    await page.waitForSelector('textarea');
-    await page.type('textarea', message);
-    await page.waitForSelector('button[type="submit"]');
-    await page.click('button[type="submit"]');
-
-    // Esperar un tiempo para asegurarse de que el mensaje se envió correctamente
-    await page.waitForTimeout(3000);
-
-    await browser.close();
+function validarFormulario() {
+    // Aquí puedes agregar restricciones adicionales si lo deseas
+    return true; // Siempre devuelve true para permitir el envío del formulario
 }
 
-// Llamar a la función para enviar un mensaje
-sendMessageToInstagram('victoriam.indu', '¡Hola! ¿Cómo estás?');
+function enviarDatosPorWhatsApp() {
+    var nombre = document.getElementById('nombre').value;
+    var apellido = document.getElementById('apellido').value;
+    var email = document.getElementById('email').value;
+    var direccion = document.getElementById('dire').value;
+    var pais = document.getElementById('pais').value;
+    var ciudad = document.getElementById('ciudad').value;
+    var postal = document.getElementById('postal').value;
+    var telefono = document.getElementById('phone').value;
+    var envio = document.querySelector('input[name="envio"]:checked').value;
+
+    var mensaje = "Nombre: " + nombre + "\n" +
+        "Apellido: " + apellido + "\n" +
+        "Email: " + email + "\n" +
+        "Dirección: " + direccion + "\n" +
+        "País: " + pais + "\n" +
+        "Ciudad: " + ciudad + "\n" +
+        "Código Postal: " + postal + "\n" +
+        "Teléfono: " + telefono + "\n" +
+        "Método de Envío: " + envio;
+
+    // Reemplaza 'NUMERO_DE_TELEFONO' con el número de teléfono al que quieres enviar el mensaje por WhatsApp
+    window.location.href = "https://wa.me/1128649573/?text=" + encodeURIComponent(mensaje);
+}
